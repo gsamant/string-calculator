bd106f0f2bde803daa374b89ba43899e92cd7a2f
Gandhali <gsamant@github.com>
2022-08-24 20:10:40 +0530

    fix

--- index.js
+++ index.js
@@ -34,7 +34,6 @@ const server = http.createServer(function(request, response) {
   }
 })
  
-const port = process.env.PORT || 3000;
-//const host = process.env.HOST ||'127.0.0.1'
-server.listen(port)
+const port = process.env.PORT || 1337;
+server.listen(port);
 console.log(`Listening at http://localhost:${port}`)
