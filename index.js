f60dd4d1e3b1b82d0e11a3388edbcec10f6fbd28
Gandhali <gsamant@github.com>
2022-08-24 19:51:11 +0530

    changed port

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const server = http.createServer(function(request, response) {
   }
 })
  
-const port = 3000
+const port = process.env.PORT || 3000;
 const host = '127.0.0.1'
 server.listen(port, host)
 console.log(`Listening at http://${host}:${port}`)
