c85b91dc99f6a2ebae624c03de63deaa00e37838
Gandhali <gsamant@github.com>
2022-08-25 09:51:42 +0530

    adding test

--- calculator.test.js
+++ calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('./calculator')
 test('string with a single number should result in the number itself', () => {
     expect(calculator.add('1')).toBe(1);
   });
+
+test('string with a single number should result in the number itself', () => {
+    expect(calculator.add('1,2,3')).toBe(6);
+  });
