c4e159073f33c9b0a8f1cd5330d92be32d04ff5b
Gandhali <gsamant@github.com>
2022-08-25 10:01:43 +0530

    adding wrong code

--- calculator.js
+++ calculator.js
@@ -1,8 +1,9 @@
 function add(numbers) {
+    console.log(numbers);
     return numbers
         .split(',')
         .map(x => parseInt(x))
-        .reduce((a, b) => a + b)
+        .reduce((a, b) => a + b +1)
 }
  
 exports.add = add;
--- calculator.test.js
+++ calculator.test.js
@@ -4,6 +4,6 @@ test('string with a single number should result in the number itself', () => {
     expect(calculator.add('1')).toBe(1);
   });
 
-test('string with a single number should result in the number itself', () => {
+test('string with a 3 numbers should result in the sum', () => {
     expect(calculator.add('1,2,3')).toBe(6);
   });
